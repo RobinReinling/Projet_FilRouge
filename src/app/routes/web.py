300127dea61861960e25ca2f51e88f25b672e907
thierno <sthsow@gmail.com>
2022-02-26 16:24:33 +0100

    maintenance_conteneur

---
+++ src/app/routes/web.py
@@ -0,0 +1,25 @@
+from flask import jsonify, render_template, Blueprint
+from src.models import models
+from src.models.models import Client
+
+web_routes = Blueprint('web', __name__)
+
+@web_routes.route('/connexion', methods=['GET'])
+def connexion():
+    return render_template('connexion.html')
+
+@web_routes.route('/inscription', methods=['GET'])
+def inscription():
+    return render_template('inscription.html')   
+
+@web_routes.route('/inscriptionfini', methods=['PUT'])
+def inscriptionfini():
+    return render_template('inscriptionfini.html')   
+
+# @web_routes.route('/test', methods=['GET'])
+# def tests():
+#     clients = Client.query.all()
+#     result = []
+#     for e in clients:
+#         result.append(e.__repr__())
+#     return jsonify(result), 200    
